f8702b4fbda6e6eeddd0d9803be3ff6fcfeccc3a
Steven Huwig <steve.huwig@unizin.org>
2016-12-05 19:02:11 -0500

    Don't update users that already exist
    This should be avoided because the mapper defines `username` in the user parameters, but Nuxeo also uses this to look up users for update.

--- unizin-login-lti-addon/src/main/java/org/unizin/cmp/lti/OAuthLTIFilter.java
+++ unizin-login-lti-addon/src/main/java/org/unizin/cmp/lti/OAuthLTIFilter.java
@@ -110,7 +110,7 @@ public class OAuthLTIFilter implements NuxeoAuthPreFilter {
 
     private static Principal principal(final Map<String, String> userObject) {
         final UserMapperService ums = Framework.getService(UserMapperService.class);
-        return ums.getOrCreateAndUpdateNuxeoPrincipal("LTI", userObject);
+        return ums.getOrCreateAndUpdateNuxeoPrincipal("LTI", true, false, userObject);
     }
 
     private static Map<String, String> parameters(final HttpServletRequest request) {
